73602dca8944f9a63da2e0b81f2de20f40a7ff0c
anamm <anamm@DESKTOP-DUV74VB>
2020-02-13 23:25:23 -0200

    refactoring do método listar classes

--- JiStar/src/main/java/br/puc/rj/jistar/model/generator/ModelGenerator.java
+++ JiStar/src/main/java/br/puc/rj/jistar/model/generator/ModelGenerator.java
@@ -32,14 +32,29 @@ import br.puc.rj.jistar.core.elements.Actor;
 public class ModelGenerator {
 
     public static void main(String args[]) {
-        try (Stream<Path> walk = Files.walk(Paths.get("C:\\class_test"))) {
+        try {
+            listClasses(args[0]);
+        } catch (Exception ex) {
+            Logger.getLogger(ModelGenerator.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    private static List<Class<Runnable>> listClasses(String javaFilesPath) throws Exception {
+        List<Class<Runnable>> classList = new ArrayList<Class<Runnable>>();
+        JavaDynamicCompiler<Runnable> compiler = new JavaDynamicCompiler<Runnable>();
+        try (Stream<Path> walk = Files.walk(Paths.get(javaFilesPath))) {
 
             List<String> result = walk.map(x -> x.toString())
                     .filter(f -> f.endsWith(".java")).collect(Collectors.toList());
 
             result.forEach(javaFilePath -> {
                 try {
-                    listClasses(javaFilePath);
+                    Path path = Paths.get(javaFilesPath);
+                    byte[] bytes = Files.readAllBytes(path);
+                    String fonte = new String(bytes, StandardCharsets.UTF_8);
+                    String nomeClasse = path.getFileName().toString().split("\\.")[0];
+                    Class<Runnable> clazz = compiler.compile(null, nomeClasse, fonte);
+
                 } catch (Exception ex) {
                     Logger.getLogger(ModelGenerator.class.getName()).log(Level.SEVERE, null, ex);
                 }
@@ -48,42 +63,6 @@ public class ModelGenerator {
         } catch (IOException ioe) {
             ioe.printStackTrace();
         }
-    }
-
-    private static void listClasses(String javaFilePath) throws Exception {
-        Path path = Paths.get(javaFilePath);
-        byte[] result = Files.readAllBytes(path);
-        String fonte = new String(result, StandardCharsets.UTF_8);
-        String nomeClasse = path.getFileName().toString().split("\\.")[0];
-        JavaDynamicCompiler<Runnable> compiler = new JavaDynamicCompiler<Runnable>();
-        
-        
-        Class<Runnable> clazz = compiler.compile(null, nomeClasse, fonte);
-        System.out.print("\nAnotado? "+clazz.isAnnotationPresent(Actor.class)+"\n\n");
-        Annotation[] annotations = clazz.getAnnotations();
-        
-        for (Annotation annotation : annotations) {
-            if (annotation instanceof Actor) {
-                Actor myAnnotation = (Actor) annotation;
-                System.out.println("name: " + myAnnotation.name());
-                System.out.println("value: " + myAnnotation.type().name());
-            }
-        }
-
-        //tentativa com javaparser
-        /*File javaFile = new File(javaFilePath);
-        //List<ClassOrInterfaceDeclaration> classes = new ArrayList<ClassOrInterfaceDeclaration>();
-        try {
-            FileInputStream inputStream = new FileInputStream(javaFilePath);
-                    CompilationUnit cu = JavaParser.parse(inputStream);
-                    cu.findAll(ClassOrInterfaceDeclaration.class).forEach(be -> {
-                        System.out.print("\n"+be.getName());
-                        be.accept(new ClassVisitor(), null);
-                    });
-        }catch (FileNotFoundException fnfEx) {
-            Logger.getLogger(ModelGenerator.class.getName()).log(Level.SEVERE, null, fnfEx);
-        } catch (Exception ex) {
-            Logger.getLogger(ModelGenerator.class.getName()).log(Level.SEVERE, null, ex);
-        }*/
+        return classList;
     }
 }
